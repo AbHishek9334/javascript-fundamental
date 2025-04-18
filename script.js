f8e0a525e7c0d6863efbdc0c655c396915dbee37
AbHishek9334 <narayana333434@gmail.com>
2025-04-18 07:30:17 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//first method of printing the data of the parent children
+// parent = document.querySelector("ul");
+// childrenCollection = parent.children; //it is a collection
+// convertedArray = Array.from(childrenCollection);
+// convertedArray.forEach((item) => {
+//   console.log(item.textContent);
+// });
+//second method of printing all the data of the parent children
+// listItem = document.querySelectorAll(".list-item");//we will get a node list
+// listItem.forEach((item) => {
+//   console.log(item.innerText);
+// });
+//how to access first and last child
+// parent = document.querySelector("ul");
+// console.log(parent.firstElementChild.innerText);
+// console.log(parent.lastElementChild.innerText);
+// const myUl = document.querySelector("ul");
+// turnGreen = myUl.querySelector("li");
+// turnGreen.style.backgroundColor = "green";
+// turnGreen.style.padding = "10px";
+//how to create an element
+// const div = document.createElement("div");
+// div.className = "main";
+// div.id = Math.round(Math.random() * 10 + 1);
+// div.setAttribute("title", "generatedText");
+// div.style.backgroundColor = "green";
+// div.style.padding = "10px";
+// const addText = document.createTextNode("chai aur code");
+// div.appendChild(addText);
+// document.body.append(div);
+// const ul = document.querySelector("ul");
+// const li = document.createElement("li");
+// const addText = document.createTextNode("chai aur code");
+// li.appendChild(addText);
+// ul.appendChild(li);
